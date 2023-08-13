b7ae2e8aea7ffa60dc7353194b1641c0dc494697
sijut0123 <seijikakitsu@icloud.com>
2023-08-14 00:25:08 +0900

    second commit

--- js1/03.js
+++ js1/03.js
@@ -3,15 +3,11 @@ const N = Number(window.prompt("自然数を入力してください"));
 /* 方法1: 和の公式を用いる
   ans1 に N (N + 1) / 2　の計算結果を代入する */
   const ans1 = (N * (N+1)) / 2;
-  console.log(ans1);
   /* 方法2: 繰り返しを用いる
   ans2 の初期値を 0 とし, 1 から N を順番に加えてゆく */
 let ans2 = 0;
 for (let i = 0; i <= N; i += 1) {
     ans2 += i;
-    if (i == N) {
-        console.log(ans2);
-    }
 }
 
 /* ２つの結果を比較
