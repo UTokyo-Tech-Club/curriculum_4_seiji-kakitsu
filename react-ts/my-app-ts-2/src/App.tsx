a422925719c4d92556cc3e2d2fecd806ed413b95
sijut0123 <seijikakitsu@icloud.com>
2023-09-03 01:13:32 +0900

    typescript2 first commit

---
+++ react-ts/my-app-ts-2/src/App.tsx
@@ -0,0 +1,55 @@
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [age, setAge] = useState(0);
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault()
+    const response = await fetch(
+      "https://test2-9881e-default-rtdb.firebaseio.com/.json",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    const obj = Object.values<{name:string, age:number}>(data).filter((v) => v.name === "inada")[0];
+    setAge(obj.age + 10);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
+            <label>Age: </label>
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(parseInt(e.target.value))}
+            ></input>
+            <button type={"submit"}>Submit</button>
+          </form>
+      </div>
+    );
+  }
+
+  export default App;
