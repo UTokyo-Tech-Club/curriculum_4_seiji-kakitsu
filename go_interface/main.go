cd1ffd1b9611a1a282cc18ad8f79e1c0a5730507
榎橘征爾 <kakitsuseiji@enokitachibanaseijinoMacBook-Air.local>
2023-12-01 11:23:53 +0900

    add MyError

--- go_interface/main.go
+++ go_interface/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -8,22 +9,22 @@ import (
 
 func main() {
 	dir := os.Args[1]
+	err := NewMyError(cd(dir), "fail: パス関連のエラーが発生しました: %s, 指定可能なサブディレクトリ: %s\n", ReadDir())
 
-	if err := cd(dir); err != nil {
-		files, _ := os.ReadDir("./")
-		var a []string
-		for _, file := range files {
-			if file.IsDir() {
-				a = append(a, file.Name())
+	if err.original != nil {
+		var pathError *os.PathError
+		if errors.As(err.original, &pathError) {
+			if ReadDir() != nil {
+				dirs := strings.Join(ReadDir(), ", ")
+				fmt.Printf(err.format, err.original.Error(), dirs)
+			} else {
+				dirs := "なし"
+				fmt.Printf(err.format, err.original.Error(), dirs)
 			}
-		}
-
-		dirs := strings.Join(a, ", ")
-		if a != nil {
-			fmt.Printf("fail: %s, 指定可能なサブディレクトリ: %s\n", err.Error(), dirs)
 		} else {
-			fmt.Printf("fail: %s, 指定可能なサブディレクトリ: なし\n", err.Error())
+			fmt.Printf("fail: エラーが発生しました： %s\n", err.original.Error())
 		}
+		return
 	} else {
 		fmt.Println("success!")
 	}
@@ -31,4 +32,29 @@ func main() {
 
 func cd(dir string) error {
 	return os.Chdir(dir)
+}
+
+func ReadDir() []string {
+	files, _ := os.ReadDir("./")
+	var a []string
+	for _, file := range files {
+		if file.IsDir() {
+			a = append(a, file.Name())
+		}
+	}
+	return a
+}
+
+type MyError struct {
+	original error
+	format   string
+	values   []any
+}
+
+func NewMyError(original error, format string, values ...any) *MyError {
+	return &MyError{
+		original: original,
+		format:   format,
+		values:   values,
+	}
 }
